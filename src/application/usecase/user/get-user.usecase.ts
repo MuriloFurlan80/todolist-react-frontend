e3b117dcb58098f6297d6ca951b061fffeb58c6a
Murilo Furlan <53433394+MuriloFurlan80@users.noreply.github.com>
2022-10-22 16:23:50 -0300

    complete implementation getUser

--- src/application/usecase/user/get-user.usecase.ts
+++ src/application/usecase/user/get-user.usecase.ts
@@ -1,8 +1,18 @@
 import { User } from "@/domain/entity/user";
 import { IGetUser } from "@/domain/usecase/user/get-user.usecase";
+import { IHttpClient } from "@/infra/http/http-client-adapter";
 
 export class GetUser implements IGetUser {
-  get(): Promise<User> {
-    throw new Error("Method not implemented.");
+  constructor(
+    private readonly http: IHttpClient,
+    private readonly url: string
+  ) {}
+
+  async get(): Promise<User> {
+    let response = await this.http.request({
+      url: this.url,
+      method: "get",
+    });
+    return response.body;
   }
 }
