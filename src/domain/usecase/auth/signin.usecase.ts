ff70d018ac7e9e9837741545e2a57c50a4d4e4b5
Murilo Furlan <53433394+MuriloFurlan80@users.noreply.github.com>
2022-10-22 19:20:57 -0300

    signin create

---
+++ src/domain/usecase/auth/signin.usecase.ts
@@ -0,0 +1,6 @@
+import { Authentication } from "@/domain/entity/auth";
+import { SigninModel } from "@/domain/model/auth/signin.model";
+
+export interface ISignin {
+    signin(data: SigninModel): Promise<Authentication>
+}
